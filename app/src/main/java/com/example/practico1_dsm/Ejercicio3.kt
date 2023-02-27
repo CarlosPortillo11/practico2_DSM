acc140e73314c130868330bf9d9bb09fdfd41132
Carlos Portillo <carloskportillo@gmail.com>
2023-02-26 19:55:33 -0600

    Ejercicio 3 finalizado

--- app/src/main/java/com/example/practico1_dsm/Ejercicio3.kt
+++ app/src/main/java/com/example/practico1_dsm/Ejercicio3.kt
@@ -5,12 +5,96 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
+import kotlin.math.truncate
+import java.math.BigDecimal
+import java.math.RoundingMode
+import java.text.DecimalFormat
 
 class Ejercicio3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ejercicio3)
+
+        val num1 = findViewById<EditText>(R.id.Number1Box);
+        val num2 = findViewById<EditText>(R.id.Number2Box);
+        val btnSuma = findViewById<Button>(R.id.btnSuma);
+        val btnResta = findViewById<Button>(R.id.btnResta);
+        val btnMultiplicacion = findViewById<Button>(R.id.btnMultiplicacion);
+        val btnDivision = findViewById<Button>(R.id.btnDivision);
+        val btnIgual = findViewById<Button>(R.id.btnIgual);
+        val btnLimpiar = findViewById<Button>(R.id.btnLimpiar);
+        val resultBox = findViewById<TextView>(R.id.resultBox);
+        val dFormat = DecimalFormat("#.##");
+        dFormat.roundingMode = RoundingMode.CEILING;
+
+        num1.setOnClickListener{
+            num1.hint = "";
+        }
+        num2.setOnClickListener{
+            num2.hint = "";
+        }
+
+        btnSuma.setOnClickListener {
+            if(num1.text.toString() == "" || num2.text.toString() == ""){
+                Toast.makeText(applicationContext, "Ingrese ambos valores antes de querer operar", Toast.LENGTH_LONG).show();
+            }
+            else{
+                val resultado = num1.text.toString().toDouble() + num2.text.toString().toDouble();
+                val parsedResultado = dFormat.format(resultado).toDouble();
+                resultBox.text = ""+parsedResultado;
+            }
+
+        }
+
+        btnResta.setOnClickListener {
+            if(num1.text.toString() == "" || num2.text.toString() == ""){
+                Toast.makeText(applicationContext, "Ingrese ambos valores antes de querer operar", Toast.LENGTH_LONG).show();
+            }
+            else{
+                val resultado = num1.text.toString().toDouble() - num2.text.toString().toDouble();
+                val parsedResultado = dFormat.format(resultado).toDouble();
+                resultBox.text = ""+parsedResultado;
+            }
+        }
+
+        btnMultiplicacion.setOnClickListener {
+            if(num1.text.toString() == "" || num2.text.toString() == ""){
+                Toast.makeText(applicationContext, "Ingrese ambos valores antes de querer operar", Toast.LENGTH_LONG).show();
+            }
+            else{
+                val resultado = num1.text.toString().toDouble() * num2.text.toString().toDouble();
+                val parsedResultado = dFormat.format(resultado).toDouble();
+                resultBox.text = ""+parsedResultado;
+            }
+        }
+
+        btnDivision.setOnClickListener {
+            if(num1.text.toString() == "" || num2.text.toString() == ""){
+                Toast.makeText(applicationContext, "Ingrese ambos valores antes de querer operar", Toast.LENGTH_LONG).show();
+            }
+            else{
+                val resultado = (num1.text.toString().toDouble())/(num2.text.toString().toDouble());
+                val parsedResultado = dFormat.format(resultado).toDouble();
+                resultBox.text = ""+parsedResultado;
+            }
+        }
+
+        btnLimpiar.setOnClickListener {
+            num1.text.clear();
+            num2.text.clear();
+            resultBox.text = ""+0;
+        }
+
+        btnIgual.setOnClickListener {
+            val toast = Toast.makeText(applicationContext, "No hago nada, pero necesitaba rellenar", Toast.LENGTH_LONG);
+            toast.show();
+        }
+
+
     }
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.main_menu, menu)
